e35601d90c606483f11f297995927a4a9c729d1d
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2024-11-10 18:05:19 +0000

    Refactor prop drilling to use context api

--- alx-react-app-props/src/App.jsx
+++ alx-react-app-props/src/App.jsx
@@ -1,27 +1,28 @@
-import WelcomeMessage from './components/WelcomeMessage'
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
-import Header from './components/Header'
-import MainContent from './components/MainContent'
-import Footer from './components/Footer'
-import UserProfile from './components/UserProfile'
+import WelcomeMessage from "./components/WelcomeMessage";
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import "./App.css";
+import Header from "./components/Header";
+import MainContent from "./components/MainContent";
+import Footer from "./components/Footer";
+import UserProfile from "./components/UserProfile";
+import ProfilePage from "./ProfilePage";
+import UserContext from "./components/UserContext";
 
 function App() {
-  const [count, setCount] = useState(0)
-
+  const [count, setCount] = useState(0);
+  const userData = { name: "Jane Doe", email: "jane.doe@example.com" };
   return (
     <>
       <WelcomeMessage />
       <Header />
       <MainContent />
       <Footer />
-      <UserProfile 
-        name="Alice"
-        age="25"
-        bio="Loves hiking and photography" 
-      />
+      <UserContext.Provider value={userData}>
+        <ProfilePage />
+      </UserContext.Provider>
+      <UserProfile name="Alice" age="25" bio="Loves hiking and photography" />
 
       <div>
         <a href="https://vite.dev" target="_blank">
@@ -44,7 +45,7 @@ function App() {
         Click on the Vite and React logos to learn more
       </p>
     </>
-  )
+  );
 }
 
-export default App
+export default App;
