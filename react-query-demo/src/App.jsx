5c7ffd247bbcf093e52b962fcc7f85ccbf180f01
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2024-11-24 19:13:18 +0000

    feat:Add component to handle fetching and caching using useQuery

---
+++ react-query-demo/src/App.jsx
@@ -0,0 +1,24 @@
+import { useState } from 'react'
+import reactLogo from './assets/react.svg'
+import viteLogo from '/vite.svg'
+import './App.css'
+import { QueryClient, QueryClientProvider } from "react-query";
+import PostsComponent from './components/PostsComponent.jsx'
+
+
+function App() {
+ 
+  return (
+    <>
+    <QueryClientProvider client={queryClient}>
+      <PostsComponent />
+    </QueryClientProvider>
+      <h1>Welcome to my React</h1>
+      <p className="read-the-docs">
+        Click on the Vite and React logos to learn more
+      </p>
+    </>
+  )
+}
+
+export default App
