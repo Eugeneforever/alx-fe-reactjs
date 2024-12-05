9e8be2c4d59d2b5ba9496a39b9e874717d097f8b
Eugene Kpakpo Acquaye <eugeneacquaye.ea6@gmail.com>
2024-12-05 16:40:36 +0000

    bug: fix a second bug in UserProfile component

--- tailwind-react-integration/src/components/UserProfile.jsx
+++ tailwind-react-integration/src/components/UserProfile.jsx
@@ -1,7 +1,7 @@
 
 const UserProfile = () => {
   return (
-   <div className="bg-gray-100 sm:text-lg max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 md:msx-w-sm md:text-xl sm:max-w-xs">
+   <div className="bg-gray-100 sm:text-lg max-w-sm mx-auto my-20 rounded-lg shadow-lg sm:p-4 md:p-8 md:max-w-sm md:text-xl sm:max-w-xs">
       <img src="https://via.placeholder.com/150" alt="User" className="rounded-full w-36 h-36 mx-auto md:w-36 md:h-36 sm:w-24 sm:h-24"/>
       <h1 className="text-xl text-blue-800 my-4">John Doe</h1>
       <p className="text-gray-600 text-base sm:text-sm md:text-base">Developer at Example Co. Loves to write code and explore new technologies.</p>
